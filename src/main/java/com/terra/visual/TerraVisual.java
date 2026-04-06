637167a9d35c84c78f2a095591c4d3befe1e4daa
Terra2975 <bdiktar@gmail.com>
2026-04-06 21:37:19 +0300

    Add TerraVisual mod initializer class

---
+++ src/main/java/com/terra/visual/TerraVisual.java
@@ -0,0 +1,15 @@
+package com.terra.visual;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TerraVisual implements ModInitializer {
+    public static final String MOD_ID = "terravisual";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Terra Visual завантажено!");
+    }
+}
